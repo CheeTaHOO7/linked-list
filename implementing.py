408e5c49eef05a169740b071001c2412a212b4de
CheeTaHOO7 <109506738+CheeTaHOO7@users.noreply.github.com>
2022-07-18 13:08:34 +0530

    Update implementing.py

--- implementing.py
+++ implementing.py
@@ -1,1 +1,17 @@
 class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkedList():
+  def __init__(self):
+    self.head = None
+   
+  def append(self, value):
+    if self.head = None:
+      self.head = Node(value)
+    else:
+      current_node = self.head
+      while current_node.next:
+        current_node = current_node.next
+      current_node.next = Node(value)
